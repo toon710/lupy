f67614cbd663b5bb2e4bf8e172591e633c0dfa99
y <y@192.168.219.193>
2022-12-01 18:24:17 +0900

    221201

---
+++ lupy/WebContent/js/w3color.js
@@ -0,0 +1,28 @@
+var colorList = [
+	'w3-red',// 첫번째 데이터 인덱스:0
+	'w3-pink',// 두번째 데이터 인덱스:1
+	'w3-purple',
+	'w3-deep-purple',
+	'w3-blue',
+	'w3-cyan',
+	'w3-aqua',
+	'w3-green',
+	'w3-light-green',
+	'w3-lime',
+	'w3-yellow',
+	'w3-amber',
+	'w3-orange',
+	'w3-deep-orange',
+	'w3-black',
+	'w3-dark-grey',
+	'w3-grey',
+	'w3-light-grey',
+	'w3-blue-grey',
+	'w3-brown',
+	'w3-pale-red',
+	'w3-sand',
+	'w3-pale-yellow',
+	'w3-khaki',
+	'w3-pale-blue',
+	'w3-light-blue'
+];
